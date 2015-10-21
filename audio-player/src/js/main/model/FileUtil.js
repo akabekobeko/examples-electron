4f6a3f39933355b92e0fa2f9b850c200f9200d4f
akabeko <akabekobeko@gmail.com>
2015-10-21 21:14:51 +0900

    #27 Add file/folder utility class

---
+++ audio-player/src/js/main/model/FileUtil.js
@@ -0,0 +1,59 @@
+import Fs from 'original-fs';
+
+/**
+ * Provide a utility of file/folder operation methods.
+ */
+export default class FileUtil {
+  /**
+   * Asynchronous mkdir(2). No arguments other than a possible exception are given to the completion callback.
+   * mode defaults to 0o777.
+   *
+   * @param  {String}   path Directory path.
+   * @param  {Function} cb   Callback function.
+   */
+  static mkdir( path, cb ) {
+    if( FileUtil.existsSync( path ) ) {
+      console.log( 'mkdir cancel exists' );
+      return cb();
+    }
+
+    Fs.mkdir( path, ( err ) => {
+      cb( err );
+    } );
+  }
+
+  /**
+   * Asynchronously writes data to a file, replacing the file if it already exists. data can be a buffer.
+   *
+   * @param  {String}      path      Write file path.
+   * @param  {ArrayBuffer} data      Target data.
+   * @param  {Function}    cb        Callback function.
+   * @param  {Boolean}     overwrite True if to overwrite the file. Default is false.
+   */
+  static writeFile( path, data, cb, overwrite ) {
+    if( !( overwrite ) && FileUtil.existsSync( path ) ) {
+      console.log( 'writeFile cancel exists' );
+      return cb();
+    }
+
+    Fs.writeFile( path, data, ( err ) => {
+      cb( err );
+    } );
+  }
+
+  /**
+   * Check the existence of a file or folder.
+   *
+   * @param {String} path Path of the file or folder.
+   *
+   * @return {Boolean} True if exists. Otherwise false.
+   */
+  static existsSync( path ) {
+    try {
+      Fs.accessSync( path, Fs.F_OK );
+      return true;
+    } catch( err ) {
+      return false;
+    }
+  }
+}
