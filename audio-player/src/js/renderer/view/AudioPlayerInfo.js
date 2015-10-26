7037948c19eb8c981c4991b16db2ebb7c4bef11d
akabeko <akabekobeko@gmail.com>
2015-10-26 21:18:08 +0900

    #32 Improved drawing processing of the spectrum analyzer
    - Spectrum analyzer to the component
    - Adjust the canvas size of the screen
    - Supports high resolution ( with window.devicePixelRatio )
    - Divide drawing of background and graphs ( Optimize CanvasContext )

--- audio-player/src/js/renderer/view/AudioPlayerInfo.js
+++ audio-player/src/js/renderer/view/AudioPlayerInfo.js
@@ -1,18 +1,6 @@
-import React    from 'react';
-import Util     from '../../common/Util.js';
-import { PlaybackState }  from '../../common/Constants.js';
-
-/**
- * Max spectrum value.
- * @type {Number}
- */
-const MAX_SPECTRUM = 255;
-
-/**
- * The margin of the upper part of the spectrum ( px ).
- * @type {Number}
- */
-const SPECTRUM_TOP_MARGIN = 5;
+import React            from 'react';
+import Util             from '../../common/Util.js';
+import SpectrumAnalyzer from './SpectrumAnalyzer.js';
 
 /**
  * Component for audio player information display.
@@ -27,35 +15,11 @@ export default class AudioPlayerInfo extends React.Component {
     super( props );
 
     /**
-     * Canvas for spectrum snalyzer.
-     * @type {HTMLCanvasElement}
-     */
-    this._canvas = null;
-
-    /**
-     * Canvas context.
-     * @type {CanvasRenderingContext2D}
-     */
-    this._canvasContext = null;
-
-    /**
-     * Identifier obtained from requestAnimationFrame.
-     * @type {Number}
-     */
-    this._animationId = null;
-
-    /**
-     * Before playback state.
-     * @type {PlaybackState}
-     */
-    this._playbackStateBefore = PlaybackState.Stopped;
-
-    /**
      * State of component.
      * @type {Object}
      */
     this.state = {
-      useSpectrumAnalizer: false
+      useSpectrumAnalyzer: false
     };
   }
 
@@ -93,7 +57,7 @@ export default class AudioPlayerInfo extends React.Component {
       info.image        = music.image;
     }
 
-    const style = { display: this.state.useSpectrumAnalizer ? 'none' : 'block' };
+    const style = { display: this.state.useSpectrumAnalyzer ? 'none' : 'block' };
 
     return (
       <div className="audio-player__container__info">
@@ -101,7 +65,10 @@ export default class AudioPlayerInfo extends React.Component {
           <div className="audio-player__container__info__container__image">
             { info.image ? ( <img src={ info.image } /> ) : null }
           </div>
-          { this._renderSpectrumAnalizer() }
+          <SpectrumAnalyzer
+            audioPlayerStore={ this.props.audioPlayerStore }
+            useSpectrumAnalyzer={ this.state.useSpectrumAnalyzer }
+            onClickInfoDisplay={ this._onClickInfoDisplay.bind( this ) } />
           <div
             className="audio-player__container__info__container__info"
             style={ style }
@@ -126,36 +93,6 @@ export default class AudioPlayerInfo extends React.Component {
   }
 
   /**
-   * Render for audio spectrum analizer.
-   *
-   * @return {ReactElement} Rendering data.
-   */
-  _renderSpectrumAnalizer() {
-    // Update spectrum analizer
-    const playbackStateNow = this.props.audioPlayerStore.playbackState;
-    if( this._playbackStateBefore !== playbackStateNow ) {
-      if( playbackStateNow === PlaybackState.Stopped ) {
-        this._stopSpectrumAnalizer();
-      } else {
-        this._updateSpectrumAnalizer();
-      }
-
-      this._playbackStateBefore = playbackStateNow;
-    }
-
-    const style = { display: this.state.useSpectrumAnalizer ? 'block' : 'none' };
-    return (
-      <div
-        className="audio-player__container__info__container__spectrum-analizer"
-        style={ style }
-        onClick={ this._onClickInfoDisplay.bind( this ) }>
-        <canvas ref="canvas">
-        </canvas>
-      </div>
-    );
-  }
-
-  /**
    * Occurs when the playback position is changed.
    *
    * @param {Event} ev Event data.
@@ -168,59 +105,6 @@ export default class AudioPlayerInfo extends React.Component {
    * Occurs when the information area is clicked.
    */
   _onClickInfoDisplay() {
-    this.setState( { useSpectrumAnalizer: !( this.state.useSpectrumAnalizer ) } );
-  }
-
-  /**
-   * Update the spectrum analizer.
-   */
-  _updateSpectrumAnalizer() {
-    const spectrums = this.props.audioPlayerStore.spectrums;
-    if( !( spectrums ) ) { return; }
-
-    this._canvasContext.clearRect( 0, 0, this._canvas.width, this._canvas.height );
-
-    const max        = spectrums.length;
-    const backHeight = this._canvas.height - SPECTRUM_TOP_MARGIN;
-    const width      = Math.round( ( this._canvas.width / max ) / 2 );
-    const nextX      = width * 2;
-
-    this._drawSpectrum( spectrums[ 0 ], 0, width, backHeight );
-    for( let i = 1; i < max; ++i ) {
-      const x = i * nextX;
-      this._drawSpectrum( spectrums[ i ], x, width, backHeight );
-    }
-
-    this._animationId = requestAnimationFrame( this._updateSpectrumAnalizer.bind( this ) );
-  }
-
-  /**
-   * Draw the background and the spectrum graph.
-   *
-   * @param  {Number} spectrum   Spectrum ( range: 0 - 255 ).
-   * @param  {Number} x          The x-coordinate of the upper-left corner of the rectangle ( px ).
-   * @param  {Number} width      The width of the rectangle ( px ).
-   * @param  {Number} backHeight The height of the background ( px ).
-   */
-  _drawSpectrum( spectrum, x, width, backHeight ) {
-    const percent = spectrum / MAX_SPECTRUM;
-    const height  = Math.round( this._canvas.height * percent ) - SPECTRUM_TOP_MARGIN;
-    const y       = ( this._canvas.height - height );
-
-    // Background
-    this._canvasContext.fillStyle = '#ecf0f1';
-    this._canvasContext.fillRect( x, SPECTRUM_TOP_MARGIN, width, backHeight );
-
-    //Bar graph
-    this._canvasContext.fillStyle = '#bdc3c7';
-    this._canvasContext.fillRect( x, y, width, height );
-  }
-
-  /**
-   * Stop the spectrum analizer.
-   */
-  _stopSpectrumAnalizer() {
-    this._canvasContext.clearRect( 0, 0, this._canvas.width, this._canvas.height );
-    cancelAnimationFrame( this._animationId );
+    this.setState( { useSpectrumAnalyzer: !( this.state.useSpectrumAnalyzer ) } );
   }
 }
---
+++ audio-player/src/js/renderer/view/SpectrumAnalyzer.js
@@ -0,0 +1,171 @@
+import React             from 'react';
+import { PlaybackState } from '../../common/Constants.js';
+
+/**
+ * Max spectrum value.
+ * @type {Number}
+ */
+const MAX_SPECTRUM = 255;
+
+/**
+ * Component for audio spectrum analyzer.
+ */
+export default class SpectrumAnalyzer extends React.Component {
+  /**
+   * Initialize instance.
+   *
+   * @param {Object} props Properties。
+   */
+  constructor( props ) {
+    super( props );
+
+    /**
+     * Canvas for spectrum snalyzer.
+     * @type {HTMLCanvasElement}
+     */
+    this._canvas = null;
+
+    /**
+     * Canvas context.
+     * @type {CanvasRenderingContext2D}
+     */
+    this._canvasContext = null;
+
+    /**
+     * Identifier obtained from requestAnimationFrame.
+     * @type {Number}
+     */
+    this._animationId = null;
+
+    /**
+     * Before playback state.
+     * @type {PlaybackState}
+     */
+    this._playbackStateBefore = props.audioPlayerStore.playbackState;
+  }
+
+  /**
+   * occur when the component did mount.
+   */
+  componentDidMount() {
+    this._canvas        = this.refs.canvas;
+    this._canvasContext = this._canvas.getContext( '2d' );
+
+    this._canvasContext.scale( window.devicePixelRatio, window.devicePixelRatio );
+  }
+
+  /**
+   * Occurs when the component is unmount.
+   */
+  componentWillUnmount() {
+    this._stopSpectrumAnayzer();
+  }
+
+  /**
+   * Render for audio spectrum analyzer.
+   *
+   * @return {ReactElement} Rendering data.
+   */
+  render() {
+    // Update spectrum analyzer
+    const playbackStateNow = this.props.audioPlayerStore.playbackState;
+    if( this._playbackStateBefore !== playbackStateNow ) {
+      if( playbackStateNow === PlaybackState.Stopped ) {
+        this._stopSpectrumAnalyzer();
+      } else {
+        this._startSpectrumAnalyzer();
+      }
+
+      this._playbackStateBefore = playbackStateNow;
+    }
+
+    const style = { display: this.props.useSpectrumAnalyzer ? 'block' : 'none' };
+    return (
+      <div
+        className="audio-player__container__info__container__spectrum-analyzer"
+        style={ style }
+        onClick={ this.props.onClickInfoDisplay }>
+        <canvas ref="canvas">
+        </canvas>
+      </div>
+    );
+  }
+
+  /**
+   * Draw the spectrum backgrounds.
+   *
+   * @param {Array.<Number>} spectrums Spectrum values ( range: 0 - 255 ).
+   * @param {Number}         baseX     The x-coordinate of the upper-left corner of the rectangle ( px ).
+   * @param {Number}         width     The width of the rectangle ( px ).
+   */
+  _drawBackground( spectrums, baseX, width ) {
+    this._canvasContext.fillStyle = '#ecf0f1';
+    this._canvasContext.fillRect( 0, 0, width, this._canvas.height );
+
+    for( let i = 1, max = spectrums.length; i < max; ++i ) {
+      this._canvasContext.fillRect( i * baseX, 0, width, this._canvas.height );
+    }
+  }
+
+  /**
+   * Draw the spectrum graphs.
+   *
+   * @param {Array.<Number>} spectrums Spectrum values ( range: 0 - 255 ).
+   * @param {Number}         baseX     The x-coordinate of the upper-left corner of the rectangle ( px ).
+   * @param {Number}         width     The width of the rectangle ( px ).
+   */
+  _drawGraph( spectrums, baseX, width ) {
+    let percent = spectrums[ 0 ] / MAX_SPECTRUM;
+    let height  = this._canvas.height * percent;
+    let y       = this._canvas.height - height;
+
+    this._canvasContext.fillStyle = '#bdc3c7';
+    this._canvasContext.fillRect( 0, y, width, height );
+
+    for( let i = 1, max = spectrums.length; i < max; ++i ) {
+      percent = spectrums[ i ] / MAX_SPECTRUM;
+      height  = this._canvas.height * percent;
+      y       = this._canvas.height - height;
+
+      this._canvasContext.fillRect( i * baseX, y, width, height );
+    }
+  }
+
+  /**
+   * Start the spectrum analyzer.
+   */
+  _startSpectrumAnalyzer() {
+    const spectrums = this.props.audioPlayerStore.spectrums;
+    if( !( spectrums ) ) { return; }
+
+    this._adjustCanvasSize();
+    this._canvasContext.clearRect( 0, 0, this._canvas.width, this._canvas.height );
+
+    const max   = spectrums.length;
+    const width = ( this._canvas.width / max ) / 2;
+    const baseX = width * 2;
+
+    this._drawBackground( spectrums, baseX, width );
+    this._drawGraph( spectrums, baseX, width );
+
+    this._animationId = requestAnimationFrame( this._startSpectrumAnalyzer.bind( this ) );
+  }
+
+  /**
+   * Stop the spectrum analyzer.
+   */
+  _stopSpectrumAnalyzer() {
+    this._canvasContext.clearRect( 0, 0, this._canvas.width, this._canvas.height );
+    cancelAnimationFrame( this._animationId );
+  }
+
+  /**
+   * Adjust the size of the Canvas on the screen.
+   */
+  _adjustCanvasSize() {
+    const width  = this._canvas.offsetWidth * window.devicePixelRatio;
+    const height = this._canvas.offsetWidth * window.devicePixelRatio;
+    if( this._canvas.width  !== width  ) { this._canvas.width  = width;  }
+    if( this._canvas.height !== height ) { this._canvas.height = height; }
+  }
+}
