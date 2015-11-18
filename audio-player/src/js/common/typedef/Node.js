c3a85d8d446ff41c37a803c64767c5c882c8a5fb
akabeko <akabekobeko@gmail.com>
2015-11-18 21:50:47 +0900

    #50 Change to "typedef" tag to "external" tag
    Type of library, the better that is linked to the document URL.

--- audio-player/src/js/common/typedef/Node.js
+++ audio-player/src/js/common/typedef/Node.js
@@ -1,17 +1,7 @@
 /**
- * Many objects in Node.js emit events.
- *
- * @see https://nodejs.org/api/events.html
- *
- * @typedef {Object} EventEmitter
+ * @external {EventEmitter} https://nodejs.org/api/events.html
  */
 
 /**
- * Pure JavaScript is Unicode friendly but not nice to binary data.
- * When dealing with TCP streams or the file system, it's necessary to handle octet streams.
- * Node.js has several strategies for manipulating, creating, and consuming octet streams. 
- *
- * @see https://nodejs.org/api/buffer.html
- *
- * @typedef {Object} Buffer
+ * @external {Buffer} https://nodejs.org/api/buffer.html
  */
