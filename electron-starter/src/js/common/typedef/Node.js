fd3a8e526b32881b8fbb10b7a219a461f7e4ff63
akabeko <akabekobeko@gmail.com>
2015-11-24 23:39:57 +0900

    #57 Electron v0.35 support

---
+++ electron-starter/src/js/common/typedef/Node.js
@@ -0,0 +1,7 @@
+/**
+ * @external {EventEmitter} https://nodejs.org/api/events.html
+ */
+
+/**
+ * @external {Buffer} https://nodejs.org/api/buffer.html
+ */
--- electron-starter/src/js/main/Main.js
+++ electron-starter/src/js/main/Main.js
@@ -1,11 +1,10 @@
-import App           from 'app';
-import CrashReporter from 'crash-reporter';
-import BrowserWindow from 'browser-window';
-import Menu          from 'menu';
-import Path          from 'path';
-import MainMenu      from './MainMenu.js';
-import MainIPC       from './MainIPC.js';
-import Util          from '../common/Util.js';
+import App             from 'app';
+import Menu            from 'menu';
+import Util            from '../common/Util.js';
+import MainMenu        from './MainMenu.js';
+import DialogManager       from './DialogManager.js';
+import { WindowTypes } from './WindowManager.js'
+import WindowManager   from './WindowManager.js';
 
 /**
  * Application entry point.
@@ -15,45 +14,68 @@ class Main {
    * Initialize instance.
    */
   constructor() {
-    /**
-     * Application main window.
-     * @type {BrowserWindow}
-     */
-    this._mainWindow  = null;
-
-    /**
-     * Manage the IPC of the main process.
-     * @type {MainIPC}
-     */
-    this._ipc = null;
-
     // Compile switch
     global.DEBUG = true;
+    if( DEBUG ) { Util.log( 'Initialize Application' ); }
+
+    /**
+     * IPC module for main process.
+     * @type {ipcMain}
+     */
+    this._ipc = require( 'electron' ).ipcMain;
+
+    /**
+     * The shell module provides functions related to desktop integration.
+     * @type {shell}
+     */
+    this._shell = require( 'electron' ).shell;
+
+    /**
+     * Manage the window.
+     * @type {WindowManager}
+     */
+    this._windowManager = new WindowManager( this );
+
+    /**
+     * Manage the native dialog.
+     * @type {DialogManager}
+     */
+    this._dialogManager = new DialogManager( this );
+  }
+
+  /**
+   * Get the IPC module.
+   * @return {ipcMain} IPC module.
+   */
+  get ipc() {
+    return this._ipc;
+  }
+
+  /**
+   * Get the shell module.
+   * @return {shell} IPC module.
+   */
+  get shell() {
+    return this._shell;
+  }
+
+  /**
+   * Get the window manager.
+   *
+   * @return {WindowManager} Instance of the window manager.
+   */
+  get windowManager() {
+    return this._windowManager;
   }
 
   /**
    * Occurs when a application launched.
    */
   onReady() {
-    if( DEBUG ) { Util.log( 'Launched' ); }
+    this._windowManager.show( WindowTypes.Main );
 
-    this._mainWindow = new BrowserWindow( {
-      width: 800,
-      height: 600,
-      resizable: true
-    } );
-
-    const filePath = Path.join( __dirname, 'index.html' );
-    this._mainWindow.loadUrl( 'file://' + filePath );
-
-    this._mainWindow.on( 'closed', () => {
-      this._mainWindow = null;
-    } );
-
-    const menu = Menu.buildFromTemplate( MainMenu.menu( this._mainWindow ) );
+    const menu = Menu.buildFromTemplate( MainMenu.menu( this ) );
     Menu.setApplicationMenu( menu );
-
-    this._ipc = new MainIPC( this._mainWindow );
   }
 
   /**
@@ -66,8 +88,18 @@ class Main {
   }
 }
 
-CrashReporter.start();
-
 const main = new Main();
-App.on( 'ready',             main.onReady           );
-App.on( 'window-all-closed', main.onWindowAllClosed );
+App.on( 'ready', () => {
+  if( DEBUG ) { Util.log( 'Application is ready' ); }
+  main.onReady();
+} );
+
+App.on( 'quit', () => {
+  if( DEBUG ) { Util.log( 'Application is quit' ); }
+} );
+
+App.on( 'window-all-closed', () => {
+  if( DEBUG ) { Util.log( 'All of the window was closed.' ); }
+
+  main.onWindowAllClosed();
+} );
