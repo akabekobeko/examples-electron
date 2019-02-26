f02dbe634fadf60ed7e58d9796cabba46232d65f
akabeko <akabekobeko@gmail.com>
2019-02-26 20:10:02 +0900

    #134 Fix a TypeScript warning

--- simple-filer/src/renderer/AppRenderer.tsx
+++ simple-filer/src/renderer/AppRenderer.tsx
@@ -6,12 +6,6 @@ import ReduxThunkMiddleware from 'redux-thunk'
 import RootReducer from './reducers/index'
 import App from './containers/App'
 
-declare global {
-  interface Window {
-    require: any
-  }
-}
-
 window.addEventListener('load', () => {
   let store = createStore(RootReducer, applyMiddleware(ReduxThunkMiddleware))
 
