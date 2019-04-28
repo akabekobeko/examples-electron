54bc2b04c324af225f8ed02e22f3632c89567675
akabeko <akabekobeko@gmail.com>
2019-04-28 21:23:20 +0900

    #141 Change the test framework from Mocha to Jest

--- electron-starter/src/renderer/reducers/updateDateTime.test.ts
+++ electron-starter/src/renderer/reducers/updateDateTime.test.ts
@@ -1,24 +1,26 @@
-import assert = require('assert')
 import { formatDate } from './updateDateTime'
 
 describe('updateDateTime', () => {
   describe('formatDate', () => {
-    it('Default YYYY-MM-DD hh:mm:ss.SSS', () => {
+    test('Default YYYY-MM-DD hh:mm:ss.SSS', () => {
       const date = new Date(2015, 7, 4, 21, 17, 45, 512)
       const actual = formatDate(date)
-      assert.deepStrictEqual(actual, '2015-08-04 21:17:45.512')
+      const expected = '2015-08-04 21:17:45.512'
+      expect(actual).toBe(expected)
     })
 
-    it('Hyphen YYYY-MM-DD-hh-mm-ss', () => {
+    test('Hyphen YYYY-MM-DD-hh-mm-ss', () => {
       const date = new Date(2015, 7, 4, 21, 17, 45, 512)
       const actual = formatDate(date, 'YYYY-MM-DD-hh-mm-ss')
-      assert.deepStrictEqual(actual, '2015-08-04-21-17-45')
+      const expected = '2015-08-04-21-17-45'
+      expect(actual).toBe(expected)
     })
 
-    it('No zero-padding YYYY/M/D h:m:s', () => {
+    test('No zero-padding YYYY/M/D h:m:s', () => {
       const date = new Date(2015, 7, 4, 21, 17, 45, 512)
       const actual = formatDate(date, 'YYYY/M/D h:m:s')
-      assert.deepStrictEqual(actual, '2015/8/4 21:17:45')
+      const expected = '2015/8/4 21:17:45'
+      expect(actual).toBe(expected)
     })
   })
 })
